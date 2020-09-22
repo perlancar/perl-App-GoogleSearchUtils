29b109167df221b1417efcf0ca52414edece9b26
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-09-22 09:09:46 +0700

    [cli google-search] Accept multiple queries

--- lib/App/GoogleSearchUtils.pm
+++ lib/App/GoogleSearchUtils.pm
@@ -1,6 +1,8 @@
 package App::GoogleSearchUtils;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
 use 5.010001;
@@ -8,16 +10,21 @@ use strict;
 use warnings;
 use Log::ger;
 
+use Perinci::Object 'envresmulti';
+
 our %SPEC;
 
 $SPEC{google_search} = {
     v => 1.1,
     summary => 'Open google search page in browser',
     args => {
-        query => {
-            schema => 'str*',
+        queries => {
+            'x.name.is_plural' => 1,
+            'x.name.singular' => 'query',
+            schema => ['array*', of=>'str*'],
             req => 1,
             pos => 0,
+            slurpy => 1,
         },
         num => {
             summary => 'Number of results per page',
@@ -32,15 +39,19 @@ sub google_search {
 
     my %args = @_;
     # XXX schema
-    my $query = $args{query} or return [400, "Please specify query"];
     my $num = $args{num} + 0;
 
-    my $url = "https://www.google.com/search?num=$num&q=".
-        URI::Escape::uri_escape($query);
-
-    my $res = Browser::Open::open_browser($url);
-
-    $res ? [500, "Failed"] : [200, "OK"];
+    my $envres = envresmulti();
+    my $i = -1;
+    for my $query (@{ $args{queries} }) {
+        $i++;
+        my $url = "https://www.google.com/search?num=$num&q=".
+            URI::Escape::uri_escape($query);
+        my $res = Browser::Open::open_browser($url);
+        $envres->add_result(
+            ($res ? (500, "Failed") : (200, "OK")), {item_id=>$i});
+    }
+    $envres->as_struct;
 }
 
 1;
